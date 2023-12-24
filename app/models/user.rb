ded420eb8a9acccc8672302ddf5260e8928badbe
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2023-12-24 06:37:00 +0000

    08end

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  belongs_to :books
+  has_many :books, dependent: :destroy
   has_one_attached :profile_image
 
-  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
 
   
   
