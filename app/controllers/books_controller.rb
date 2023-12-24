57a8d4cd46bc10266f37f3d34b9c146c3e8bf8d4
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2023-12-24 09:38:18 +0000

    end

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+before_action :correct_user, only: [:edit, :update]
 
   def show
   @book = Book.find(params[:id])
@@ -48,4 +49,12 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+  
+  def correct_user
+    @book = Book.find(params[:id])
+    unless current_user == @book.user
+      redirect_to(books_path)
+    end
+  end
+  
 end
