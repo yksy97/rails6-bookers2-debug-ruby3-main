3bb3c97bc18f0c2f9e9b9587b96f9432ff2f63f5
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2023-12-26 08:05:12 +0000

    follow_start

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,24 @@
+class RelationshipsController < ApplicationController
+   before_action :authenticate_user
+   
+  def create
+    following = @current_user.follow(params[:user_id])
+    redirect_to("/users/#{params[:user_id]}")
+  end
+
+  def destroy
+    following = @current_user.unfollow(params[:user_id])
+    redirect_to("/users/#{params[:user_id]}")
+  end
+
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.followers
+  end
+  
+end
